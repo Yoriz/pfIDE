66f2944e914b92b58fdf4c7a44dfbc9198b0e5d4
Somelauw <Somelauw>
2012-08-01 21:32:24 +0200

    Proper test?

--- test/test_autocomplete.py
+++ test/test_autocomplete.py
@@ -13,18 +13,20 @@ class TestCodeCompletion(unittest.TestCase):
         pass
 
     def test_suggest(self):
-        suggestions = self.autocomplete.suggest("math")
+        suggestions = self.autocomplete.suggest("m")
         self.assertIn("math", suggestions)
 
     def test_suggest2(self):
-        suggestions = self.autocomplete.suggest("sin")
+        suggestions = self.autocomplete.suggest("s")
         self.assertIn("sin", suggestions)
         # TODO: maybe it should only suggest "sin" if I enter math.sin
 
     def test_clear(self):
+        for char in "math":
+            self.autocomplete.update_key(char)
         self.autocomplete.clear()
-        suggestions = self.autocomplete.suggest("si")
-        self.assertEqual(suggestions, [])
+        suggestions = self.autocomplete.suggest()
+        self.assertIn("math", suggestions)
 
         # AssertionError: set(['signal', 'sin', 'sinh']) != []
 
