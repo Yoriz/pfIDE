78fec82b4742266269349496a32cded5737bc1b8
Somelauw <Somelauw>
2012-08-02 18:13:53 +0200

    Added extra unit tests for autocomplete.
    One of the unit tests tests issue #12.
    The other tests tests for case insensitivity.

--- test/test_autocomplete.py
+++ test/test_autocomplete.py
@@ -13,17 +13,45 @@ class TestCodeCompletion(unittest.TestCase):
         pass
 
     def test_suggest(self):
+        """Test autocompletion module name"""
         suggestions = self.autocomplete.suggest("m")
         self.assertIn("math", suggestions)
 
     def test_suggest2(self):
+        """Test autocompleting function name"""
         suggestions = self.autocomplete.suggest("s")
         self.assertIn("sin", suggestions)
-        # TODO: maybe it should only suggest "sin" if I enter math.sin
+
+    def test_suggest3(self):
+        """Only autocomplete incomplete keywords"""
+        suggestions = self.autocomplete.suggest("sin")
+        self.assertNotIn("sin", suggestions)
+
+    def test_case_insensitive(self):
+        """Test whether autocomplete is case sensitive"""
+        suggestions = self.autocomplete.suggest("MaT")
+        self.assertIn("math", suggestions)
+
+    def test_interactive(self):
+        """A more interative unit test"""
+        for char in "mat":
+            self.autocomplete.update_key(char)
+        suggestions = self.autocomplete.suggest()
+        self.assertIn("math", suggestions)
+
+    def test_back(self):
+        """Removing chars"""
+        for char in "matXX":
+            self.autocomplete.update_key(char)
+        self.autocomplete.back()
+        self.autocomplete.back()
+        suggestions = self.autocomplete.suggest()
+        self.assertIn("math", suggestions)
 
     def test_clear(self):
-        for char in "math":
+        for char in "mathematicals":
             self.autocomplete.update_key(char)
+        self.clear()
         suggestions = self.autocomplete.suggest()
         self.assertIn("math", suggestions)
 
